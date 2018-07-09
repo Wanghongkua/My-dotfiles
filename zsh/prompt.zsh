59150b98283b2f9e9b5665d0acc867f3358c4f23
Wanghongkua <728261774@qq.com>
2018-07-09 15:53:08 +1000

    add ssh prompt

--- zsh/prompt.zsh
+++ zsh/prompt.zsh
@@ -1,9 +1,15 @@
 ZSH_THEME_GIT_PROMPT_PREFIX="%{$reset_color%}%{$fg[green]%}"
 ZSH_THEME_GIT_PROMPT_SUFFIX="%{$reset_color%} "
-ZSH_THEME_GIT_PROMPT_DIRTY="%{$fg[yellow]%}✗"
 
 # This is an issue caused by the terminal system display
 # ZSH_THEME_GIT_PROMPT_DIRTY="%{$fg[yellow]%}⚡"
+ZSH_THEME_GIT_PROMPT_DIRTY="%{$fg[yellow]%}✗"
 ZSH_THEME_GIT_PROMPT_CLEAN=""
 
-PROMPT='%{$fg[cyan]%}%1~%{$reset_color%}%{$fg[red]%}|%{$reset_color%}$(git_prompt_info)%{$fg[cyan]%}⇒%{$reset_color%} '
+if [[ -z "$SSH_CLIENT" ]]; then
+    prompt_host=""
+else
+    prompt_host="%{$reset_color%}%{$fg[yellow]%}$(hostname -s)%{$reset_color%}%{$fg[red]%}|"
+fi
+
+PROMPT='${prompt_host}%{$fg[cyan]%}%1~%{$reset_color%}%{$fg[red]%}|%{$reset_color%}$(git_prompt_info)%{$fg[cyan]%}⇒%{$reset_color%} '
